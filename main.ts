a2b23a17e959c42d63f746d218d58b1144bfe014
Alexander Borkowski <alex-b@opus-b.com>
2020-09-24 19:42:45 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.onGesture(Gesture.TiltRight, function () {
+    if (User_Key == Key && Lock == 1) {
+    	
+    }
+})
 function Password () {
     User_Key = Temp
     serial.writeLine(User_Key)
@@ -17,29 +22,17 @@ function Password () {
             basic.clearScreen()
             User_Key = ""
             Temp = ""
+            Count = Count + 1
+            if (Count > Try) {
+                control.waitMicros(25000)
+                control.reset()
+            }
         }
     }
 }
 touchbit.on(touchbit.TouchPad.b, touchbit.TouchEvent.pressed, function () {
     if (User_Key == Key && Lock == 1) {
         radio.sendNumber(2)
-    }
-})
-input.onButtonPressed(Button.A, function () {
-    if (Lock == 0) {
-        Temp = "" + User_Key + "A"
-        basic.showString("A")
-        Password()
-    }
-})
-input.onGesture(Gesture.LogoUp, function () {
-    if (User_Key == Hand && Lock == 1) {
-    	
-    }
-})
-touchbit.on(touchbit.TouchPad.d, touchbit.TouchEvent.pressed, function () {
-    if (User_Key == Key && Lock == 1) {
-        radio.sendNumber(4)
     }
 })
 input.onGesture(Gesture.TiltLeft, function () {
@@ -50,6 +43,30 @@ input.onGesture(Gesture.TiltLeft, function () {
 input.onGesture(Gesture.ScreenUp, function () {
     if (User_Key == Hand && Lock == 1) {
     	
+    }
+})
+input.onGesture(Gesture.LogoUp, function () {
+    if (User_Key == Hand && Lock == 1) {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (Lock == 0) {
+        Temp = "" + User_Key + "A"
+        basic.showString("A")
+        basic.pause(1000)
+        basic.clearScreen()
+        Password()
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if ((User_Key == Key || User_Key == Hand) && Lock == 1) {
+        radio.sendNumber(8)
+    }
+})
+touchbit.on(touchbit.TouchPad.d, touchbit.TouchEvent.pressed, function () {
+    if (User_Key == Key && Lock == 1) {
+        radio.sendNumber(4)
     }
 })
 touchbit.on(touchbit.TouchPad.right, touchbit.TouchEvent.pressed, function () {
@@ -67,6 +84,11 @@ input.onGesture(Gesture.ScreenDown, function () {
     	
     }
 })
+input.onGesture(Gesture.LogoDown, function () {
+    if (User_Key == Key && Lock == 1) {
+    	
+    }
+})
 touchbit.on(touchbit.TouchPad.c, touchbit.TouchEvent.pressed, function () {
     if (User_Key == Key && Lock == 1) {
         radio.sendNumber(3)
@@ -81,22 +103,9 @@ input.onButtonPressed(Button.B, function () {
     if (Lock == 0) {
         Temp = "" + User_Key + "B"
         basic.showString("B")
+        basic.pause(1000)
+        basic.clearScreen()
         Password()
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    if ((User_Key == Key || User_Key == Hand) && Lock == 1) {
-        radio.sendNumber(8)
-    }
-})
-input.onGesture(Gesture.TiltRight, function () {
-    if (User_Key == Key && Lock == 1) {
-    	
-    }
-})
-input.onGesture(Gesture.LogoDown, function () {
-    if (User_Key == Key && Lock == 1) {
-    	
     }
 })
 touchbit.on(touchbit.TouchPad.a, touchbit.TouchEvent.pressed, function () {
@@ -109,33 +118,36 @@ input.onGesture(Gesture.ThreeG, function () {
     	
     }
 })
+let Count = 0
 let Temp = ""
 let Lock = 0
 let User_Key = ""
+let Try = 0
 let Hand = ""
 let Key = ""
 radio.setGroup(1)
 Key = "ABBA"
 Hand = "BAAB"
+Try = 5
 User_Key = ""
 Lock = 0
 Temp = ""
 basic.forever(function () {
     if (Temp == "") {
         led.toggle(0, 4)
-        basic.pause(200)
+        basic.pause(500)
         basic.clearScreen()
         led.toggle(1, 4)
-        basic.pause(200)
+        basic.pause(500)
         basic.clearScreen()
         led.toggle(2, 4)
-        basic.pause(200)
+        basic.pause(500)
         basic.clearScreen()
         led.toggle(3, 4)
-        basic.pause(200)
+        basic.pause(500)
         basic.clearScreen()
         led.toggle(4, 4)
-        basic.pause(200)
+        basic.pause(500)
         basic.clearScreen()
     }
 })
